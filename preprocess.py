845279900cbcfbb9e8df78a16cf34db4acaf5b16
gursi26 <gursimar.batra@gmail.com>
2021-03-29 23:24:37 +0800

    dataset collected

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,5 @@
 import cv2
 import mediapipe as mp 
-import numpy as np
 import pandas as pd 
 
 # ---------------------------------------------- Single run ----------------------------------------- #
@@ -15,8 +14,8 @@ mpHands = mp.solutions.hands
 hands = mpHands.Hands(
     static_image_mode=False,
     max_num_hands=2,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5
+    min_detection_confidence=0.8,
+    min_tracking_confidence=0.8
 )
 mp_draw = mp.solutions.drawing_utils
 
@@ -46,27 +45,38 @@ lm_list = [
 
 # ---------------------------------------------- Loop -------------------------------------------- #
 
-img_path = '/Users/gursi/Desktop/ASL/asl_dataset/A/A627.jpg'
-frame = cv2.imread(img_path)
+vid = cv2.VideoCapture(0)
+action = 'nothing'
 
-img_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-results = hands.process(img_rgb)
+try : 
+    while True : 
 
-letter = 'sample'
+        success, frame = vid.read()
+        img_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = hands.process(img_rgb)
 
-if results.multi_hand_landmarks : 
-    for hand_landmarks in results.multi_hand_landmarks :
-        landmark_list = [letter]
-        mp_draw.draw_landmarks(frame, hand_landmarks, mpHands.HAND_CONNECTIONS)
+        if results.multi_hand_landmarks : 
+            for hand_landmarks in results.multi_hand_landmarks :
+                landmark_list = [action]
+                mp_draw.draw_landmarks(frame, hand_landmarks, mpHands.HAND_CONNECTIONS)
 
-        for lm in lm_list : 
-            lms = hand_landmarks.landmark[lm]
-            landmark_list.append(lms.x)
-            landmark_list.append(lms.y)
-            landmark_list.append(lms.z)
+                for lm in lm_list : 
+                    lms = hand_landmarks.landmark[lm]
+                    landmark_list.append(lms.x)
+                    landmark_list.append(lms.y)
+                    landmark_list.append(lms.z)
 
-        landmark_df = pd.DataFrame([landmark_list], columns=cols)
-        dataset = dataset.append(landmark_df)
+                landmark_df = pd.DataFrame([landmark_list], columns=cols)
+                dataset = dataset.append(landmark_df)
+        
+        cv2.imshow('output', frame)
+        if cv2.waitKey(1) and 0xFF == ord('q'):
+            break
+
+except KeyboardInterrupt : 
+    pass
+
+dataset.to_csv(f'{action}.csv')
             
 
 
