01d90ebbe9fb952c3dca008419798e8c449af6fe
Solomon1732 <shlomireu5@gmail.com>
2021-10-02 13:06:26 +0300

    Extract lists outside of functions
    Extracted lists outside of functions. Before the extraction the lists were instantiated each time the functions were called. In addition, the functions themselves are called from loops. This means that the lists are needlessly created and destroyed each time the functions are called. Extracting the lists allows them to be created once and used repeatedly.

--- main.py
+++ main.py
@@ -25,36 +25,35 @@ hands = mpHands.Hands(
 )
 mp_draw = mp.solutions.drawing_utils
 
+_lm_list = [
+mpHands.HandLandmark.WRIST, 
+mpHands.HandLandmark.THUMB_CMC, 
+mpHands.HandLandmark.THUMB_MCP,
+mpHands.HandLandmark.THUMB_IP, 
+mpHands.HandLandmark.THUMB_TIP, 
+mpHands.HandLandmark.INDEX_FINGER_MCP,
+mpHands.HandLandmark.INDEX_FINGER_DIP, 
+mpHands.HandLandmark.INDEX_FINGER_PIP, 
+mpHands.HandLandmark.INDEX_FINGER_TIP,
+mpHands.HandLandmark.MIDDLE_FINGER_MCP,
+mpHands.HandLandmark.MIDDLE_FINGER_DIP, 
+mpHands.HandLandmark.MIDDLE_FINGER_PIP, 
+mpHands.HandLandmark.MIDDLE_FINGER_TIP, 
+mpHands.HandLandmark.RING_FINGER_MCP, 
+mpHands.HandLandmark.RING_FINGER_DIP,
+mpHands.HandLandmark.RING_FINGER_PIP, 
+mpHands.HandLandmark.RING_FINGER_TIP, 
+mpHands.HandLandmark.PINKY_MCP,
+mpHands.HandLandmark.PINKY_DIP, 
+mpHands.HandLandmark.PINKY_PIP, 
+mpHands.HandLandmark.PINKY_TIP
+]
 
 ## Extract landmark positions as array 
 def landmark_extract(hand_lms, mpHands):
     output_lms = []
 
-    lm_list = [
-    mpHands.HandLandmark.WRIST, 
-    mpHands.HandLandmark.THUMB_CMC, 
-    mpHands.HandLandmark.THUMB_MCP,
-    mpHands.HandLandmark.THUMB_IP, 
-    mpHands.HandLandmark.THUMB_TIP, 
-    mpHands.HandLandmark.INDEX_FINGER_MCP,
-    mpHands.HandLandmark.INDEX_FINGER_DIP, 
-    mpHands.HandLandmark.INDEX_FINGER_PIP, 
-    mpHands.HandLandmark.INDEX_FINGER_TIP,
-    mpHands.HandLandmark.MIDDLE_FINGER_MCP,
-    mpHands.HandLandmark.MIDDLE_FINGER_DIP, 
-    mpHands.HandLandmark.MIDDLE_FINGER_PIP, 
-    mpHands.HandLandmark.MIDDLE_FINGER_TIP, 
-    mpHands.HandLandmark.RING_FINGER_MCP, 
-    mpHands.HandLandmark.RING_FINGER_DIP,
-    mpHands.HandLandmark.RING_FINGER_PIP, 
-    mpHands.HandLandmark.RING_FINGER_TIP, 
-    mpHands.HandLandmark.PINKY_MCP,
-    mpHands.HandLandmark.PINKY_DIP, 
-    mpHands.HandLandmark.PINKY_PIP, 
-    mpHands.HandLandmark.PINKY_TIP
-    ]
-
-    for lm in lm_list : 
+    for lm in _lm_list : 
         lms = hand_lms.landmark[lm]
         output_lms.append(lms.x)
         output_lms.append(lms.y)
