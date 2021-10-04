bb076852ac0a28c4f960f25a7bffa982b70f96f4
Neeraj  Anand <78193865+neerajanand-coder@users.noreply.github.com>
2021-10-04 20:34:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import cv2, torch
 import time, os
-import mediapipe as mp 
+import mediapipe as mp
 import numpy as np
-from model import Model 
+from model import Model
 
 current_path = os.getcwd()
 
@@ -15,8 +15,8 @@ min_conf = 0.75
 max_hands = 2
 # Path of trained model. Can be changed to point to a custom model
 model_path = os.path.join(current_path, 'models/120.pt')
-# Pen parameters 
-pen_color = (255,0,0)
+# Pen parameters
+pen_color = (255, 0, 0)
 eraser_size = 80
 pen_size = 10
 # The density of the line. Smaller values make the line more smooth.
@@ -24,18 +24,32 @@ intermediate_step_gap = 4
 # Create Control window to change color and size of pen
 cv2.namedWindow('control')
 # This show the color
-img = np.zeros((100, 600, 3), np.uint8)
+img = np.zeros((200, 600, 3), np.uint8)
 
 
 def nothing(x):
     pass
+
+
 # This create trackbar to adjust various values.
 cv2.createTrackbar('Red', 'control', 0, 255, nothing)
 cv2.createTrackbar('Blue', 'control', 0, 255, nothing)
 cv2.createTrackbar('Green', 'control', 0, 255, nothing)
 cv2.createTrackbar('pen_thickness', 'control', 0, 30, nothing)
-switch = 'final_img_save: NO\n 1: YES'
-cv2.createTrackbar(switch, 'control', 0, 1, nothing)
+cv2.createTrackbar('Imd_step_gap', 'control', 2, 50, nothing)
+button = [20, 60, 145, 460]
+
+
+def process_click(event, x, y, flags, params):
+    # check if the click is within the dimensions of the button
+    if event == cv2.EVENT_LBUTTONDOWN:
+        if button[0] < y < button[1] and button[2] < x < button[3]:
+            cv2.imwrite('Image' + str(fps) + '.png', frame)
+            img[:80, :] = (0, 0, 255)
+            print('Clicked on Button!')
+
+
+cv2.setMouseCallback('control', process_click)
 cap = cv2.VideoCapture(cam_number)
 
 mpHands = mp.solutions.hands
@@ -48,34 +62,35 @@ hands = mpHands.Hands(
 mp_draw = mp.solutions.drawing_utils
 
 _lm_list = [
-    mpHands.HandLandmark.WRIST, 
-    mpHands.HandLandmark.THUMB_CMC, 
+    mpHands.HandLandmark.WRIST,
+    mpHands.HandLandmark.THUMB_CMC,
     mpHands.HandLandmark.THUMB_MCP,
-    mpHands.HandLandmark.THUMB_IP, 
-    mpHands.HandLandmark.THUMB_TIP, 
+    mpHands.HandLandmark.THUMB_IP,
+    mpHands.HandLandmark.THUMB_TIP,
     mpHands.HandLandmark.INDEX_FINGER_MCP,
-    mpHands.HandLandmark.INDEX_FINGER_DIP, 
-    mpHands.HandLandmark.INDEX_FINGER_PIP, 
+    mpHands.HandLandmark.INDEX_FINGER_DIP,
+    mpHands.HandLandmark.INDEX_FINGER_PIP,
     mpHands.HandLandmark.INDEX_FINGER_TIP,
     mpHands.HandLandmark.MIDDLE_FINGER_MCP,
-    mpHands.HandLandmark.MIDDLE_FINGER_DIP, 
-    mpHands.HandLandmark.MIDDLE_FINGER_PIP, 
-    mpHands.HandLandmark.MIDDLE_FINGER_TIP, 
-    mpHands.HandLandmark.RING_FINGER_MCP, 
+    mpHands.HandLandmark.MIDDLE_FINGER_DIP,
+    mpHands.HandLandmark.MIDDLE_FINGER_PIP,
+    mpHands.HandLandmark.MIDDLE_FINGER_TIP,
+    mpHands.HandLandmark.RING_FINGER_MCP,
     mpHands.HandLandmark.RING_FINGER_DIP,
-    mpHands.HandLandmark.RING_FINGER_PIP, 
-    mpHands.HandLandmark.RING_FINGER_TIP, 
+    mpHands.HandLandmark.RING_FINGER_PIP,
+    mpHands.HandLandmark.RING_FINGER_TIP,
     mpHands.HandLandmark.PINKY_MCP,
-    mpHands.HandLandmark.PINKY_DIP, 
-    mpHands.HandLandmark.PINKY_PIP, 
+    mpHands.HandLandmark.PINKY_DIP,
+    mpHands.HandLandmark.PINKY_PIP,
     mpHands.HandLandmark.PINKY_TIP
 ]
 
-## Extract landmark positions as array 
+
+## Extract landmark positions as array
 def landmark_extract(hand_lms, mpHands):
     output_lms = []
 
-    for lm in _lm_list : 
+    for lm in _lm_list:
         lms = hand_lms.landmark[lm]
         output_lms.append(lms.x)
         output_lms.append(lms.y)
@@ -87,8 +102,8 @@ def landmark_extract(hand_lms, mpHands):
 ## Checks if the position is out of bounds or not
 def is_position_out_of_bounds(position, top_left, bottom_right):
     return (
-        position[0] > top_left[0] and position[0] < bottom_right[0]
-        and position[1] > top_left[1] and position[1] < bottom_right[1]
+            position[0] > top_left[0] and position[0] < bottom_right[0]
+            and position[1] > top_left[1] and position[1] < bottom_right[1]
     )
 
 
@@ -97,63 +112,67 @@ model = Model()
 model.load_state_dict(torch.load(model_path, map_location='cpu'))
 model.eval()
 
-action_map = {0:'Draw', 1:'Erase', 2:'None'}
+action_map = {0: 'Draw', 1: 'Erase', 2: 'None'}
 
 ## cv2 text parameters
 font = cv2.FONT_HERSHEY_SIMPLEX
 fontScale = 1
-fontColor = (255,255,255)
+fontColor = (255, 255, 255)
 lineType = 4
-
 ## Stores previously drawn circles to give continous lines
 circles = []
-
+params = []
 was_drawing_last_frame = False
 
 ptime = 0
 ctime = 0
 
 ## Video feed loop
-while True : 
+while True:
     success, frame = cap.read()
-    if flip : 
-        frame = cv2.flip(frame,1)
-    h,w,c = frame.shape
+    if flip:
+        frame = cv2.flip(frame, 1)
+    h, w, c = frame.shape
     img_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     results = hands.process(img_rgb)
-    cv2.rectangle(frame, (w,h), (w-320,h-90), (0,0,0), -1, 1)
+    cv2.rectangle(frame, (w, h), (w - 320, h - 90), (0, 0, 0), -1, 1)
     b = cv2.getTrackbarPos('Blue', 'control')
     g = cv2.getTrackbarPos('Green', 'control')
     r = cv2.getTrackbarPos('Red', 'control')
     t = cv2.getTrackbarPos('pen_thickness', 'control')
-    s = cv2.getTrackbarPos(switch, 'control')
-    pen_color = (b, g, r)
-    pen_size = t
-    if not results.multi_hand_landmarks :
+    imd_step_gap = cv2.getTrackbarPos('Imd_step_gap', 'control')+1
+    #print(imd_step_gap)
+    # pen_color = (b, g, r)
+    # pen_size = t
+    intermediate_step_gap = imd_step_gap
+    if not results.multi_hand_landmarks:
         was_drawing_last_frame = False
-        cv2.putText(frame, 'No hand in frame', (w-300, h-50), font, fontScale, fontColor, lineType)
-    else : 
-        for hand_landmarks in results.multi_hand_landmarks :
+        cv2.putText(frame, 'No hand in frame', (w - 300, h - 50), font, fontScale, fontColor, lineType)
+    else:
+        for hand_landmarks in results.multi_hand_landmarks:
             mp_draw.draw_landmarks(frame, hand_landmarks, mpHands.HAND_CONNECTIONS)
 
-            ## Mode check 
+            ## Mode check
             landmark_list = landmark_extract(hand_landmarks, mpHands)
             model_input = torch.tensor(landmark_list, dtype=torch.float).unsqueeze(0)
             action = action_map[torch.argmax(model.forward(model_input)).item()]
-            cv2.putText(frame, f'Mode : {action}', (w-300, h-50), font, fontScale, fontColor, lineType)
+            cv2.putText(frame, f'Mode : {action}', (w - 300, h - 50), font, fontScale, fontColor, lineType)
 
             ## Draw mode
-            if action == 'Draw': 
+            if action == 'Draw':
+                pen_color = (b, g, r)
+                pen_size = t
                 index_x = hand_landmarks.landmark[mpHands.HandLandmark.INDEX_FINGER_TIP].x
                 index_y = hand_landmarks.landmark[mpHands.HandLandmark.INDEX_FINGER_TIP].y
-                pos = (int(index_x*w), int(index_y*h))
-                cv2.circle(frame, pos, 20, (255,0,0), 2)
+                pos = (int(index_x * w), int(index_y * h))
+                cv2.circle(frame, pos, 20, (255, 0, 0), 2)
                 if was_drawing_last_frame:
                     prev_pos = circles[-1]
                     x_distance = pos[0] - prev_pos[0]
                     y_distance = pos[1] - prev_pos[1]
                     distance = (x_distance ** 2 + y_distance ** 2) ** 0.5
                     num_step_points = int(distance) // intermediate_step_gap - 1
+                    print(num_step_points)
                     if num_step_points > 0:
                         x_normalized = x_distance / distance
                         y_normalized = y_distance / distance
@@ -163,6 +182,7 @@ while True :
                             step_pos = (step_pos_x, step_pos_y)
                             circles.append(step_pos)
                 circles.append(pos)
+                params.append((pos, pen_color, pen_size))
                 was_drawing_last_frame = True
             else:
                 was_drawing_last_frame = False
@@ -170,37 +190,37 @@ while True :
             ## Erase mode
             if action == 'Erase':
                 eraser_mid = [
-                        int(hand_landmarks.landmark[mpHands.HandLandmark.MIDDLE_FINGER_MCP].x * w),
-                        int(hand_landmarks.landmark[mpHands.HandLandmark.MIDDLE_FINGER_MCP].y * h)
-                    ]
+                    int(hand_landmarks.landmark[mpHands.HandLandmark.MIDDLE_FINGER_MCP].x * w),
+                    int(hand_landmarks.landmark[mpHands.HandLandmark.MIDDLE_FINGER_MCP].y * h)
+                ]
 
-                bottom_right = (eraser_mid[0]+eraser_size, eraser_mid[1]+eraser_size)
-                top_left = (eraser_mid[0]-eraser_size, eraser_mid[1]-eraser_size)
+                bottom_right = (eraser_mid[0] + eraser_size, eraser_mid[1] + eraser_size)
+                top_left = (eraser_mid[0] - eraser_size, eraser_mid[1] - eraser_size)
 
-                cv2.rectangle(frame, top_left, bottom_right, (0,0,255), 5)
-                
+                cv2.rectangle(frame, top_left, bottom_right, (0, 0, 255), 5)
+
                 circles = [
                     position
                     for position in circles
                     if not is_position_out_of_bounds(position, top_left, bottom_right)
                 ]
 
-    ## Draws all stored circles 
-    for position in circles : 
+    ## Draws all stored circles
+    for position, pen_color, pen_size in params:
         frame = cv2.circle(frame, position, pen_size, pen_color, -1)
-    
+
     ctime = time.time()
-    fps = round(1/(ctime-ptime),2)
+    fps = round(1 / (ctime - ptime), 2)
     ptime = ctime
-    cv2.putText(frame, f'FPS : {fps}', (w-300, h-20), font, fontScale, fontColor, lineType)
+    cv2.putText(frame, f'FPS : {fps}', (w - 300, h - 20), font, fontScale, fontColor, lineType)
 
     cv2.imshow('output', frame)
+    # contol_image = img[:80, :]
+    img[button[0]:button[1], button[2]:button[3]] = 180
+    cv2.putText(img, 'Click_to_save_img', (148, 50), cv2.FONT_HERSHEY_TRIPLEX, 1.0, 0, 1)
     cv2.imshow('control', img)
-    img[:] = [b, g, r]
-    # for saving final image and quit.
-    if s == 1:
-        cv2.imwrite("Image" + str(fps) + ".png", frame)
-        break
-    if cv2.waitKey(1) and 0xFF == ord('q'):
-            break
+    img[:80, :] = [255, 255, 255]
+    img[80:, :] = [b, g, r]
 
+    if cv2.waitKey(1) and 0xFF == ord('q'):
+        break
